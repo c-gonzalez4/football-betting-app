7b4da0f6dd6a0ef1a31b61e642f238cd987f9260
Carolina Gonzalez <rpcgonzalez@gmail.com>
2021-01-07 17:43:20 -0500

    Coding Challenge #3

--- script.js
+++ script.js
@@ -95,3 +95,41 @@ for (const [ team, odd ] of Object.entries(game.odds)) {
 	//console.log(team, odd);
 	console.log(`Odd of ${teamStr} ${odd}`);
 }
+
+//Coding challenge #3
+
+const gameEvents = new Map([
+	[ 17, '⚽ GOAL' ],
+	[ 36, '� Substitution' ],
+	[ 47, '⚽ GOAL' ],
+	[ 61, '� Substitution' ],
+	[ 64, '� Yellow card' ],
+	[ 69, '� Red card' ],
+	[ 70, '� Substitution' ],
+	[ 72, '� Substitution' ],
+	[ 76, '⚽ GOAL' ],
+	[ 80, '⚽ GOAL' ],
+	[ 92, '� Yellow card' ]
+]);
+
+//create array of the different evants that happened(no duplicates)
+const events = [ ...new Set(gameEvents.values()) ];
+console.log(events);
+
+//delete yellow card from minute 64
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//calculate the average between every event
+console.log(`An event happened, on
+average, every ${90 / gameEvents.size} minutes`);
+console.log();
+//console.log(gameEvents.keys());
+for (const [ key, value ] of gameEvents) {
+	//console.log(key, value);
+	if (key <= 45) {
+		console.log(`[FIRST HALF] ${key}: ${value}`);
+	} else {
+		console.log(`[SECOND HALF] ${key}: ${value}`);
+	}
+}
